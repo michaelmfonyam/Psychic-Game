178f56a547812f0dd69b3f18e0fcf5fcf2c0ceda
unknown <mike_aze@yahoo.ca>
2018-05-21 20:43:28 -0400

    <changes>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,49 @@
-var targetLetter = "";//empty string
+var Fletter = "";
 var wins = 0;
 var losses = 0;
-var remainingGuesses = 9;//loading page with max. number before before fully loading
+var Guessesremaining = 9;
 var guessedSoFar = [];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
-$(document).ready(function () { //document loads before entire page
+$(document).ready(function () { 
     pickTargetLetter();
 
-    $(document).keypress(function (event) {//keypress evokes checkGuess function with value "guess"
-        //alert(event.key);
-        var guess = event.key;//variable = value
+    $(document).keypress(function (event) {
+        
+        var guess = event.key;
         checkGuess(guess);
     });
 });
 
-function checkGuess(aGuess) {
-    if (aGuess === targetLetter) { // you win
+function checkGuess(G) {
+    if (G === Fletter) { 
         wins += 1
-        remainingGuesses = 9;
+        Guessesremaining = 9;
         pickTargetLetter();
         guessedSoFar = [];
     } else {
-        remainingGuesses -= 1;
-        if (remainingGuesses === 0) { // you lost
+        Guessesremaining -= 1;
+        if (Guessesremaining === 0) { 
             losses += 1;
-            remainingGuesses = 9;
+            Guessesremaining = 9;
             pickTargetLetter();
             guessedSoFar = [];
         } else {
-            guessedSoFar.push(aGuess);
+            guessedSoFar.push(G);
         }
     }
     updateScreen();
 }
 
-function pickTargetLetter() {//converting my numbers to letters
+function pickTargetLetter() {
     var whichNumber = Math.floor(Math.random() * (25 - 0 + 1)) + 0;
-    targetLetter = letters[whichNumber];
+    Fletter = letters[whichNumber];
 }
 
-function updateScreen() {//updating screen after each win or loss
+function updateScreen() {
     $("#wins").text("Wins: " + wins);
     $("#losses").text("Losses: " + losses);
-    $("#remaining").text("Guesses Left: " + remainingGuesses);
+    $("#remaining").text("Guesses Left: " + Guessesremaining);
     $("#guessed").text("Your Guesses So Far: " + guessedSoFar);
 }
